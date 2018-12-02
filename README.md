99f63eb3b59d60c686bc940e8d8d51226b2c1a09
Ryan Fahsel <rfahsel3@Ryans-MacBook-Pro.local>
2018-12-01 18:32:07 -0800

    - Updating the README - GIving the extension a proper command name.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-TODO
+# generateuuid README
+
+A simple Visual Studio Code extension to translate Base64 encoded JWTs into their human-readable claims. 
+
+## Features
+
+This library only has one command.
+* `Decode JWT` -> Select this command when you have highlighted a JWT. If it's a legit JWT, then the claims will be printed below it.
+
+## Requirements
+
+No requirements.
+
+## Extension Settings
+
+None
+
+## Known Issues
+
+None yet, but I am sure they are hiding ready to be reported.
+
+## Release Notes
+
+Inital releases.
--- extension.js
+++ extension.js
@@ -14,7 +14,7 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.whatTheJWT', function () {
         var getCurrentCursorRange = function() {
             var textEditor = vscode.window.activeTextEditor;
             var currentSelection = textEditor.selection;
